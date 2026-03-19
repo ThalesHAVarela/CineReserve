2a5618ebf036e96d87627d5a00790c01d63106e6
ThalesHAVarela <thales2004varela@gmail.com>
2026-03-18 23:42:17 -0300

    adiciona models (Movie, Session, Seat, Reservation,Ticket)

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    age_rating = models.IntegerField()
+    duration = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Session(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    saloon = models.CharField(max_length=100)
+    total_seats = models.IntegerField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Seat(models.Model):
+    STATUS_CHOICES = [
+        ('available', 'Available'),
+        ('reserved', 'Reserved'),
+        ('purchased', 'Purchased'),
+    ]
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    seat_number = models.IntegerField()
+    seat_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default= 'available')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
+
+def get_expiry():
+    return timezone.now() + timedelta(minutes=10)
+
+class Reservation(models.Model):
+    status_choices = [
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+        ('pending', 'Pending'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=status_choices, default='pending')
+    expires_at = models.DateTimeField(default=get_expiry)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+import uuid
+
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat,on_delete=models.CASCADE)
+    code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
