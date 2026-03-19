1abe03aeaf016f8dd37d5ac4296224fe0ff52ab3
ThalesHAVarela <thales2004varela@gmail.com>
2026-03-19 16:17:37 -0300

    adiciona views

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .models import Movie, Session, Seat, Reservation, Ticket
+from .serializers import MovieSerializer, SessionSerializer, SeatSerializer, ReservationSerializer, TicketSerializer
 # Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class SessionViewSet(viewsets.ModelViewSet):
+    queryset = Session.objects.all()
+    serializer_class = SessionSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    
